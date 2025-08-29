7839d4651164298daa6e3b72ab314d14f28a18bb
Anastasia <anast111antonova@gmail.com>
2025-08-29 22:45:09 +0300

    Add functions for calculation buttons

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+
+class CalculatorLayout(BoxLayout):
+    def digit_button(self, digit):
+        print(f'Number {digit}')
+
+    def operstion_button(self, op):
+        print(f'Number {op}')
+
+    def clear_button(self):
+        print('Clear')
+
+    def equals_button(self):
+        print('Equals')
+
+    def decimal_button(self):
+        print('Decimal point')
+
+class CalculatorApp(App):
+    def build(self):
+        return CalculatorLayout()
+
+if __name__ == '__main__':
+    CalculatorApp().run()
